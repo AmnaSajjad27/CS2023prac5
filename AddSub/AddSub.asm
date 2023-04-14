c0a9521108dc3e217e5682527413d2284e31dfd4
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-14 22:02:40 +0930

    AddSub

--- AddSub/AddSub.asm
+++ AddSub/AddSub.asm
@@ -2,3 +2,22 @@
 // (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], and RAM[3], respectively.)
 
 // Put your code here.
+
+// initalise R1 and store it in D
+@R1
+D = M
+// initalise R2
+@R2
+// R1 + R2
+D = D + M
+// initalise R3
+@R3
+// ...- R3
+D = D - M
+// store it in R0
+@R0
+M = D
+// end the program 
+(END)
+@END
+0;JMP
