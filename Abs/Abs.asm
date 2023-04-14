383b0d6b3514d43c3d2921d771f48cf05e5dfe21
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-14 22:56:55 +0930

    Abs

--- Abs/Abs.asm
+++ Abs/Abs.asm
@@ -2,3 +2,29 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+
+// initalise R1 and hold it in D
+@R1
+D = M
+
+// if negative 
+@IFTRUE
+D;JLT
+
+// if positive 
+@R0
+M = D
+
+// end the program
+@END
+D;JMP
+
+// Absolute value 
+(IFTRUE)
+@R0
+M = -D
+
+// end the program
+(END)
+@END
+0;JMP
