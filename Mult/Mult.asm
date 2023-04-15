590ff4885ad202cd49cd037cb1b36f37b9a832b9
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-15 15:30:47 +0930

    it works

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -7,3 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// initalise R0 to 0
+@R0
+M = 0
+
+// jump if (R2 > 0)
+@R2
+D = M
+@STEP
+D;JGT
+
+// else go to end
+@END
+0;JMP
+
+// Succesively add R1 to R2 to *multiply*
+// if R2 > 0, we step again 
+(STEP)
+@R0
+D = M
+
+// adding here 
+@R1
+D = D + M
+
+// writting the result back to R0
+@R0
+M = D
+
+// now we reduce our counter i.e. R2 by 1
+@R2
+D = M - 1
+M = D 
+
+// if R2 > 0 still, loop again
+@STEP
+D;JGT
+
+(END)
+@R0
+0;JMP
