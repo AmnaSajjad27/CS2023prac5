abd3467d625e2b33a3d3e1c1f52e3d02b91ca2fa
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-15 15:43:23 +0930

    huh

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -7,53 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-
-// initalise R0 to 0
 @R0
-M = 0
-
-@R1
-D = M
-
-@END
-D;JEQ
-
-@R2
-D = M
-@END
-D;JEQ
-
-// jump to neg loop 
-@NEG
-0;JMP
+    M=0
+    @R1
+    D=M
+    @END
+    D;JEQ
+    @R2
+    D=M
+    @END
+    D;JEQ
+    @NEG
+    0;JMP
 
 (LOOP)
-@R1
-D = M
-
-@R0
-// Succesively add to multiply 
-M = M + D
-@R2
-
-MD = M - 1
-@LOOP
-D;JGT
-
+    @R1
+    D=M
+    @R0
+    M=M+D
+    @R2
+    MD=M-1
+    @LOOP
+    D;JGT
 (END)
-@END
-0;JMP
+    @END
+    0;JMP
 
-// NEG loop
-// Succesively adding negative numbers 
 (NEG)
-@R2
-D = M
-@LOOP
-D;JGE
-@R1
-M = -M
-@R2
-M = - MD
-@LOOP
-0;JMP
+    @R2
+    D=M
+    @LOOP
+    D;JGE
+    @R1
+    M=-M
+    @R2
+    M=-M
+    @LOOP
+    0;JMP
