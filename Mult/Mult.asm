a8419836b24640e5116d8aae651b1c9a17e64ede
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-15 16:04:39 +0930

    commented

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -7,40 +7,54 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// initalise R0 to zero 
 @R0
-    M=0
-    @R1
-    D=M
-    @END
-    D;JEQ
-    @R2
-    D=M
-    @END
-    D;JEQ
-    @NEG
-    0;JMP
+M=0
 
+// get R1 and R2
+@R1
+D=M
+@END
+D;JEQ
+
+@R2
+D=M
+@END
+D;JEQ
+
+// jump to NEG loop if input is negative
+@NEG
+0;JMP
+
+// loop to successively add positive intgers and multiply 
 (LOOP)
-    @R1
-    D=M
-    @R0
-    M=M+D
-    @R2
-    MD=M-1
-    @LOOP
-    D;JGT
-(END)
-    @END
-    0;JMP
+@R1
+D=M
+@R0
+M=M+D
+@R2
+// minus 1 into the counter and keep adding 
+MD=M-1
+@LOOP
+D;JGT
 
+(END)
+@END
+0;JMP
+
+// NEG loop 
 (NEG)
-    @R2
-    D=M
-    @LOOP
-    D;JGE
-    @R1
-    M=-M
-    @R2
-    M=-M
-    @LOOP
-    0;JMP
+@R2
+D=M
+@LOOP
+D;JGE
+@R1
+// absolute value to calc the product
+M=-M
+@R2
+// put the negative back in the output
+M=-M
+
+@LOOP
+0;JMP
