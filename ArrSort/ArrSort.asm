c458b99f3043f6a37cbaf2bf55f7903fb585a5b0
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-16 16:13:28 +0930

    cmp files

--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -2,3 +2,117 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M = 0
+
+// outer loop
+(OUTTER)
+@j
+M = 0
+
+// inner loop 
+// check if the elemnt stored a x is less than or more than element stored in y
+(INNER)
+// *(R1+j)
+@R1
+D=M
+@j
+A = M + D
+D = A
+@x
+M = D
+@y
+M = D + 1
+@x
+A = M
+D = M
+
+@NEGX
+D;JLT
+@POSX
+D;JGT
+
+(COMPARE)
+@x
+A = M
+D = M
+@y
+A = M
+D = D - M
+@SWAP
+D;JGT
+
+// After the inner loop completes a full pass over the array, the code checks if the value of j is greater than or equal to R2-i-1
+// where i is the current iteration of the outer loop. If this condition is true, the inner loop is terminated.
+
+(CHECK)
+// check if (j >= R2-i-1)
+@i
+D = M
+@R2
+D = D + 1
+D = M - D
+@j
+M = M + 1
+D = D - M
+@INNER
+D;JGT
+
+// check if (i >= R2-1)
+@i
+M = M + 1
+D = M
+@R2
+D = M - D
+D = D - 1
+@OUTTER
+D;JGT
+
+(END)
+@R0
+M = -1
+@END
+0;JMP
+
+// swaploop, jumps here if elements need to be swaped 
+(SWAP)
+@x
+A = M
+D = M
+@R4
+M = D
+
+@y
+A = M
+D = M
+@x
+A = M
+M = D
+
+@R4
+D = M
+@y
+A = M
+M = D
+
+@CHECK
+0;JMP
+
+(NEGX)
+@y
+A = M
+D=M
+@CHECK
+D;JGT
+@COMPARE
+0;JMP
+
+(POSX)
+@y
+A = M
+D = M
+@SWAP
+D;JLT
+@COMPARE
+0;JMP
